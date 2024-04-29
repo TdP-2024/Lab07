fc13f0d93ec953318c40b8f2d1c41f9a63b125f2
cmas1 <car.masone@gmail.com>
2024-04-29 10:05:42 +0200

    soluzione in aula

--- UI/controller.py
+++ UI/controller.py
@@ -14,12 +14,26 @@ class Controller:
         self._mese = 0
 
     def handle_umidita_media(self, e):
-        pass
-
+        if self._mese == 0:
+            self._view.create_alert("Selezionare un mese")
+            return
+        results = self._model.calcola_umidita_media(self._mese)
+        self._view.lst_result.controls.clear()
+        for result in results:
+            self._view.lst_result.controls.append(ft.Text(f"{result[0]} -> {result[1]}"))
+        self._view.update_page()
 
 
     def handle_sequenza(self, e):
-        pass
+        if self._mese == 0:
+            self._view.create_alert("Selezionare un mese")
+            return
+        sequenza, costo = self._model.calcola_sequenza(self._mese)
+        self._view.lst_result.controls.clear()
+        self._view.lst_result.controls.append(ft.Text(f"Il costo della sequenza è {costo}"))
+        for fermata in sequenza:
+            self._view.lst_result.controls.append(ft.Text(fermata))
+
 
     def read_mese(self, e):
         self._mese = int(e.control.value)
